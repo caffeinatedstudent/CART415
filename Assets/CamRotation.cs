e4b3408ad8b754384eff50bfcefe30ba556a9943
Edwin Lucu <e_ucu@cda122ev5635x.local>
2018-01-29 14:51:48 -0500

    new camera

---
+++ Assets/CamRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamRotation : MonoBehaviour 
+{
+	private float x;
+	private float y;
+
+
+	void Update()
+	{
+		float rotateX = Random.Range (0, 50);
+		transform.eulerAngles = new Vector3(0,0,rotateX);
+	}
+}
