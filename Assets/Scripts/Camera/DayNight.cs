248b5073820661dd0b5aefbaccbab06d3031616d
Edwin Lucu <e_ucu@cda100ev5715x.local>
2018-01-29 12:02:00 -0500

    Fixed enemy spawn and reset rotation of directional light

--- Assets/Scripts/Camera/DayNight.cs
+++ Assets/Scripts/Camera/DayNight.cs
@@ -3,68 +3,96 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+namespace Complete
+{
+
+    public class DayNight : MonoBehaviour
+    {
+        public TankManager[] tankManagers;
+        //public List<GameObject> gameObjects;
+        public List<Transform> waypointsForAI;
+        public GameObject tankPrefab;
+        float coco;
+        public Color32 color32 = new Color32(32, 27, 32, 53);
+        public bool ColorOn = false;
+		public Quaternion originalRotationValue;
+
+        private AudioSource audioSource;
+        public AudioClip anthem;
+
+        void Start()
+        {
+			originalRotationValue = transform.rotation;
+        }
 
 
-public class DayNight : MonoBehaviour {
-	public GameObject darkSpawn;
-	public GameObject darkSpawn2;
-	public GameObject darkSpawn3;
-	public List<GameObject> gameObjects;
-	public GameObject tank;
-	float coco;
-	public Color32 color32 = new Color32(32, 27,32,53);
-	public bool ColorOn = false;
-
-
-	private AudioSource audioSource;
-	public AudioClip anthem;
-
-	void Start() {
-		this.gameObjects = new List<GameObject> () { darkSpawn, darkSpawn2, darkSpawn3 };
-	}
-
-
-	// Use this for initialization
-//	public class IfStatements : MonoBehaviour{
+        // Use this for initialization
+        //	public class IfStatements : MonoBehaviour{
 
 
 
-	// Update is called once per frame
-	void Update () {
-		
-		RotationCheck ();
-		transform.Rotate(new Vector3(0.08f,0f,0f));
-		if (ColorOn == false) {
-			RenderSettings.ambientLight = color32;
-		} else if (ColorOn == true) {
-			RenderSettings.ambientLight = Color.red;
-			}
-	}
+        // Update is called once per frame
+        void Update()
+        {
+
+            RotationCheck();
+            transform.Rotate(new Vector3(0.08f, 0f, 0f));
+            if (ColorOn == false)
+            {
+                RenderSettings.ambientLight = color32;
+            }
+            else if (ColorOn == true)
+            {
+                RenderSettings.ambientLight = Color.red;
+            }
+        }
 
 
-		void RotationCheck ()
-		{
-		
-		this.coco = transform.localEulerAngles.x;
-		Debug.Log(this.coco);
+        void RotationCheck()
+        {
+
+            this.coco = transform.localEulerAngles.x;
+            Debug.Log(this.coco);
 
 
-		if (Math.Round(coco,5) == 21.99551) {
-				foreach (GameObject obj in gameObjects) {
-					Instantiate (tank, obj.transform.position, Quaternion.identity);
-						ColorOn = true;
-							audioSource = GetComponent<AudioSource>();
-								audioSource.clip = anthem;
-									audioSource.Play();
+			if (Math.Round(coco, 5) == 21.99551) 
+            {
 
-				}
-		}
-		if (Math.Round(coco,4) == 318.7615){
-				ColorOn = false;
+                for (int i = 0; i < tankManagers.Length; i++) {
+                    tankManagers[i].m_Instance = Instantiate(tankPrefab, tankManagers[i].m_SpawnPoint.position, tankManagers[i].m_SpawnPoint.rotation) as GameObject;
+                    tankManagers[i].m_PlayerNumber = i + 10;
+                    tankManagers[i].SetupAI(waypointsForAI);
+                }
 
-			}
-		}
-	}
+                ColorOn = true;
+                audioSource = GetComponent<AudioSource>();
+                audioSource.clip = anthem;
+                audioSource.Play();
 
 
-//}
+                //foreach (GameObject obj in gameObjects)
+                //{
+                //    GameObject newTank = Instantiate(tank, obj.transform.position, Quaternion.identity) as GameObject;
+                //    StateController controller = newTank.GetComponent<StateController>();
+
+                //    controller.SetupAI(waypointsForAI);
+
+
+
+
+                //}
+            }
+            if (Math.Round(coco, 4) == 318.7615)
+            {
+                ColorOn = false;
+				transform.rotation = originalRotationValue;
+				//this.transform.rotation.x = 53;
+				//gameObject.transform.rotation = originalPos;
+            }
+        }
+    }
+
+
+    //}
+
+}
