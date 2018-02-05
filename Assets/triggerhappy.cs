c2ce0b4fff5d9f93855f5f19e1ac8c225cec6c67
Edwin Lucu <e_ucu@cda122ev5635x.local>
2018-02-05 16:33:11 -0500

    Start

---
+++ Assets/triggerhappy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerhappy : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject door; // This is where you reference the door you want to destroy
+
+	void OnCollisionEnter(Collision other) {
+
+		if(other.collider.tag == "Player") {
+			Destroy(door);
+			 //This will destroy the pressure plate after it has been triggered 
+		}
+	}
+}
