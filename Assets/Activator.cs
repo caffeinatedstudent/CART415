c1ee024ea3dac8806361f5310f75ba7f885bde3f
Edwin Lucu <e_ucu@cda123ev5635x.local>
2018-02-14 17:15:06 -0500

    ok

--- Assets/Activator.cs
+++ Assets/Activator.cs
@@ -5,58 +5,89 @@ using UnityEngine;
 public class Activator : MonoBehaviour {
 
 
-//	SpriteRenderer sr;
-	//public Color oldColor;
-	//public Color altColor = Color.black;
-	//public Renderer rend;
+	public AudioSource Scored;
+
+	public Color oldColor;
+
+	public bool inTrigger;
+	public bool isPlaying;
 
 	public KeyCode key;
 	bool active = false;
 	GameObject note;
-	public GameObject explosion;
-	public List<Transform> TankParts;
+	public Transform Effect_Explosion;
 
+	public GameObject bodyColor;
+	public GameObject turretColor;
 	void Awake(){
-//		sr = GetComponent<SpriteRenderer> ();
-	//	rend = GetComponent<Renderer> ();
-	//	rend.material.color = oldColor;
+
+		oldColor = bodyColor.GetComponentInChildren<MeshRenderer> ().material.color;
+
 	}
 	// Use this for initialization
 	void Start () {
-		
+		Scored = GetComponent<AudioSource> ();
+		inTrigger = false;
+		isPlaying = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+	  
 		if(Input.GetKeyDown(key))
 			
 		//Assign the changed color to the material.
 		StartCoroutine (Pressed ());
-		Instantiate (explosion, transform.position, transform.rotation);
+		
+		if (Input.GetKeyUp (key)) {
+			if (isPlaying) {
+				// turn music OFF
+				Scored.Pause ();
+
+				isPlaying = false;
+			}
+			inTrigger = false;
+		
+		}
 		if (Input.GetKeyDown (key) && active) {
+			if (inTrigger == true) {
+				Scored.Play();
+				isPlaying = true;
+			}
+			Instantiate (Effect_Explosion, transform.position, Quaternion.identity);
 			Destroy (note);
 		}
-		
+
 	}
 
 	void OnTriggerEnter(Collider col){
 		active = true;
 		if (col.gameObject.tag == "Note")
 			note = col.gameObject;
+		inTrigger = true;
+
 	}
 
 	void OnTriggerExit(Collider col){
 		active = false;
+		inTrigger = false;
+		isPlaying = false;
+
 	}
 
 	IEnumerator Pressed(){
-	//	gameObject.GetComponent<Renderer> ().enabled = false;
-
-	//	rend.material.color = altColor;
+		bodyColor.GetComponentInChildren<MeshRenderer>().material.color = new Color(0f,0f,0f,0f);
+		turretColor.GetComponentInChildren<MeshRenderer>().material.color = new Color(0f,0f,0f,0f);
+ 
 	yield return new WaitForSeconds (0.2f);
-	//	rend.material.color = oldColor;
-		//GetComponent<Renderer> ().enabled = true;
-
+		bodyColor.GetComponentInChildren<MeshRenderer>().material.color = oldColor;
+		turretColor.GetComponentInChildren<MeshRenderer>().material.color = oldColor;
+	
 
 	}	
 }
+//j.dbap_bformat
+//miraweb
+//MAXADUINO
+// ml.star
